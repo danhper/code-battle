9ae889b9a839328f08a17ddaee3b8767f960d44e
Daniel Perez <tuvistavie@gmail.com>
2013-10-08 21:28:59 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dmtc::Application.config.secret_token = 'c6d6136c68598063a97c05231bc50159d4a5f8081b542ef78e9616bc4d35b426cfd03c558226efe1f2136d6e04d5af0e89e8a90750b5cfb247555def4adb9cfe'
