3e722a4f2f01bd5d885966f663ce719ff9484f8e
Daniel Perez <tuvistavie@gmail.com>
2013-10-13 01:21:12 +0900

    Add basic logic to post code.

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,22 +1,29 @@
 class CodesController < ApplicationController
+  before_action :set_quest
   def index
   end
 
   def new
-    @problem = Problem.find(params[:problem_id])
-    @code = @problem.codes.build(params[:code])
+    @code = @quest.codes.build(params[:code])
+  end
+
+  def show
   end
 
   def create
     @code = Code.new(code_params)
     if @code.save
-      redirect_to @code
+      redirect_to [@quest, @code]
     else
       render action: :new
     end
   end
 
   private
+  def set_quest
+    @quest = Quest.find(params[:quest_id])
+  end
+
   def set_code
     @code = Code.find(params[:id])
   end
