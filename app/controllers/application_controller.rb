29bd2a244ecdc09b872e31eb237aca29de0fb201
Daniel Perez <tuvistavie@gmail.com>
2013-10-13 05:00:02 +0900

    Fix redirection after login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      if resource.username.blank?
+        edit_user_path(resource)
+      else
+        request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+      end
+    end
+  end
+
   def authenticate_user_with_username!
     authenticate_user!
     redirect_to edit_user_path(current_user) if current_user.username.blank?
