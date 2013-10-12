af0bf313f7dd2f3fcaa2be51f69797eb9c9d8c2d
0673nC <0673nC@0673nC-no-MacBook-Air.local>
2013-10-13 03:44:46 +0900

    rank

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -18,20 +18,34 @@ class Quest < ActiveRecord::Base
   has_many :quest_total_votes
   belongs_to :creator, foreign_key: :creator_id, class_name: 'User'
 
-  def GetQuestTotalVote _quest_id
+  def GetGuildCoefficient gid
+    return 1.0 - (Guild.count(:users, :conditions => ["guild_id = gid"]) / Guild.count(:users))
+  end
+
+  def GetQuestTotalVotePoint qid
     h = Hash.new
     h.default = 0
-
-    QuestTotalVote.where(:quest_id => _quest_id).find_each do |i|
-      h[i.voting_id]
+    QuestTotalVote.where(:quest_id => qid).find_each do |i|
+      h[i.voting_id] += i.vote_num * GetGuildCoefficient(i.voted_id)
     end
-    
-    # Guild.select("guild_id").find_each do |voting_gid|
-    #   Guild.select("guild_id").find_each do |voted_gid|
-        
-    #   end  
-    # end
-    
 
+    return h
+  end
+
+  def GetTotalVotePoint
+    h = Hash.new
+    h.default = 0
+    TotalVote.find_each do |i|
+      h[i.voting_id] += i.vote_num * GetGuildCoefficient(i.voted_id)
+    end
+  end
+
+  def GetQuestGuildTop qid
+    h = Hash.new
+    h.default = 0
+    Guild.find_each do |g|
+      h[g.ids] =
+        Code.where(:quest_id => qid, :guild_id => g.ids).order("users DESC").first
+    end
   end
 end
