21741aba54bddcf346160208c783a5d0c2cfeb0a
Daniel Perez <tuvistavie@gmail.com>
2013-12-06 01:41:18 +0900

    Update config files to improve automatic deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, '/home/codebattle/code-battle'
 set :log_level, :debug
 # set :pty, true
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/settings/production.local.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
