4a1915b678a3c02c0d67772ef9f4798fbe39ed4a
Daniel Perez <tuvistavie@gmail.com>
2013-12-08 21:13:08 +0900

    Add comment remove functionality.

--- app/assets/javascripts/views/comments/comment.js.coffee
+++ app/assets/javascripts/views/comments/comment.js.coffee
@@ -3,6 +3,18 @@ class Dmtc.Views.Comment extends Backbone.View
 
   initialize: (options) ->
 
+  events:
+    'click .delete': 'removeComment'
+
+  removeComment: (e) ->
+    e.preventDefault()
+    if confirm I18n.t('comment.confirm_delete')
+      @model.destroy(
+        success: (e) =>
+          @trigger 'removed'
+          @remove()
+      )
+
   render: ->
-    @$el.html @template(@model.toJSON())
+    @$el.html @template(@model.toObject())
     this
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user_with_username!, only: [:create]
+  before_action :authenticate_user_with_username!, only: [:create, :destroy]
+  before_action :set_comment, only: [:destroy]
+  before_action :check_user!, only: [:destroy]
 
   def create
     @comment = current_user.comments.build(comment_params)
@@ -10,9 +12,24 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment.destroy
+    head :no_content
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:content, :code_id)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def check_user!
+    if @comment.user_id != current_user.id
+      render json: { error: 'not authorized'}.to_json, status: 403
+    end
   end
 
 end
