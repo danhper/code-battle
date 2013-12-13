54ab4d81ffc8a326184802e3cc3c580619a6d5b5
Daniel Perez <tuvistavie@gmail.com>
2013-12-14 01:41:10 +0900

    Add data udpate to migration.

--- db/migrate/20131205080036_add_voting_guild_id_to_vote.rb
+++ db/migrate/20131205080036_add_voting_guild_id_to_vote.rb
@@ -1,5 +1,8 @@
 class AddVotingGuildIdToVote < ActiveRecord::Migration
   def change
     add_column :votes, :voting_guild_id, :integer
+    Vote.all.each do |v|
+      v.update_attribute :voting_guild_id, v.user.large_guild
+    end
   end
 end
