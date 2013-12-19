4a88e695a2d48f97c4230b26ce02cb0a5a114b7f
Daniel Perez <tuvistavie@gmail.com>
2013-12-19 14:53:51 +0900

    Fix migration to avoid unwanted exception.

--- db/migrate/20131217063653_add_role_to_user.rb
+++ db/migrate/20131217063653_add_role_to_user.rb
@@ -1,6 +1,6 @@
 class AddRoleToUser < ActiveRecord::Migration
   def change
     add_column :users, :role, :string, default: 'normal'
-    User.find_by(username: "tuvistavie").update(role: :admin)
+    User.find_by(username: "tuvistavie").update(role: :admin) rescue nil
   end
 end
